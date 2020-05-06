036ca8b8464904089a617e18d9b6fe7d4f84f7c9
William Go <williamgilbertgo@gmail.com>
2020-05-06 11:59:38 -0700

    Added images

--- backend/models/order.js
+++ backend/models/order.js
@@ -2,21 +2,16 @@ const mongoose = require("mongoose");
 
 // Build the schema for Order
 const orderSchema = new mongoose.Schema({
-  pizza: {
-    name: String,
-    crust: String,
-    size: String,
-    price: Number,
-    toppings: String
-  },
+  pizza: String,
+  image: String,
+  toppings: [String],
+  size: String,
+  crust: String,
   quantity: Number,
-  customer: {
-    customerName: String,
-    phone: String,
-    address: String
-  },
-  createdOn: { type: Date, default: Date.now }
+  price: Number,
+  createdOn: { type: Date, default: Date.now },
+  customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer' }
 });
 
-// Create the Order model
+// Create and export the Order model
 module.exports = mongoose.model("Order", orderSchema);
